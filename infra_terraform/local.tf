ec2179636aaa4e07e03737fc55e2ff1d45525a96
Fatma Gamal <104701248+FatmaGamal7@users.noreply.github.com>
2026-01-31 00:36:00 +0200

    Update local.tf

--- infra_terraform/local.tf
+++ infra_terraform/local.tf
@@ -1,10 +1,4 @@
 locals {
-#   vpc_cidr     = "10.0.0.0/16"
-#   public_cidr_1  = "10.0.1.0/24"
-#   public_cidr_2  = "10.0.2.0/24"
-#   private_cidr_1 = "10.0.101.0/24"
-#   private_cidr_2 = "10.0.102.0/24"
-
   public_cidr  = var.env == "prod" ? var.public_cidr_prod : var.public_cidr_nonprod
   private_cidr = var.env == "prod" ? var.private_cidr_prod : var.private_cidr_nonprod
 }
